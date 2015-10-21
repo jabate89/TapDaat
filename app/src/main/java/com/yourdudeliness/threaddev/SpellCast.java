3f1735f1b58b03f6c787acd422cc57ed7a32a680
Julius Abate <julius.abate@comcast.net>
2015-10-20 18:29:16 -0700

    Updated method which prints values to buttons
    Portrait locked the UI
    
    CP1 now works properly (click value will increase
    during spell duration if you increase passive income)

---
+++ app/src/main/java/com/yourdudeliness/threaddev/SpellCast.java
@@ -0,0 +1,41 @@
+package com.yourdudeliness.threaddev;
+
+import android.os.CountDownTimer;
+
+/**
+ * Created by Awesomeness on 10/20/15.
+ */
+public class SpellCast {
+
+
+    public static void testUpgradeBonus(){
+
+
+        new CountDownTimer(15000, 200) {
+
+            public void onTick(long millisUntilFinished) {
+                    primary_activity.cp1.setEnabled(false);
+                    MainActivity.currClickVal = MainActivity.baseClickVal + (10 * (int)MainActivity.currPassive);
+
+
+
+                }
+
+            public void onFinish() {
+        MainActivity.currClickVal = MainActivity.baseClickVal;
+                primary_activity.cp1.setEnabled(true);
+
+            }
+        }.start();
+
+
+
+
+    }
+
+
+
+
+
+
+}
