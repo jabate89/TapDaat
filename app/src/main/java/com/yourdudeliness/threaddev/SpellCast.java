3ade48e6b522514d982ef8799ba0ca4bd7c851d7
Julius Abate <julius.abate@comcast.net>
2015-11-30 18:45:26 -0800

    Touched up the spells so they run cleanly, reset the original values properly
    Lowered the building costs for pathos buildings, so
    gameplay is more fun. They were too expensive as is,
    and we don't have sufficient upgrades for user to gain
    that much money yet

--- app/src/main/java/com/yourdudeliness/threaddev/SpellCast.java
+++ app/src/main/java/com/yourdudeliness/threaddev/SpellCast.java
@@ -18,20 +18,24 @@ public class SpellCast {
 
     }
 
+    /*
+    Increase the value per click by 5 * currentPassive, for a duration of 15 seconds
+     */
     public static void power2(){
         MainActivity.currMana -= 600;
+        final double valHolder = MainActivity.baseClickVal; //holds the original click value to be reset at the end
         new CountDownTimer(15000, 200) {
 
             public void onTick(long millisUntilFinished) {
-                primary_activity.cp2.setEnabled(false);
-                MainActivity.currClickVal = MainActivity.baseClickVal + (10 * (int)MainActivity.currPassive);
+                primary_activity.cp2.setEnabled(false);//disable the button so spell cannot run twice at once
+                MainActivity.baseClickVal += (5 * (int)MainActivity.currPassive);
 
 
 
             }
 
             public void onFinish() {
-                MainActivity.currClickVal = MainActivity.baseClickVal;
+                MainActivity.baseClickVal = valHolder;
                 primary_activity.cp2.setEnabled(true);
 
             }
@@ -42,19 +46,19 @@ public class SpellCast {
     public static void power3(){
 
         MainActivity.currMana -= 750;
-
-        new CountDownTimer(15000, 200) {
+        final double valHolder = MainActivity.currPassive;
+        new CountDownTimer(10000, 200) {
 
             public void onTick(long millisUntilFinished) {
                 primary_activity.cp3.setEnabled(false);
-                MainActivity.currClickVal = MainActivity.baseClickVal + (10 * (int)MainActivity.currPassive);
+                MainActivity.currPassive *= 15;
 
 
 
             }
 
             public void onFinish() {
-                MainActivity.currClickVal = MainActivity.baseClickVal;
+                MainActivity.currPassive = valHolder;
                 primary_activity.cp3.setEnabled(true);
 
             }
